48d533c0194428a90f38f4a033c05299c507c9fb
Delia <Suirravin40@outlook.com>
2022-09-26 10:55:12 -0400

    added more queries

--- SQL Queries/entire_collection.sql
+++ SQL Queries/entire_collection.sql
@@ -17,9 +17,24 @@ SELECT season_num season_number, count(season_num) total_wesen_first_appearances
 -- Query first appearances individually by each season.
 SELECT wesen_type, season_num 
   FROM episodes
- WHERE season_num = 1
+ WHERE season_num = 1;
  
-
+ -- Query to look for wesens with certain types of characteristics
+ SELECT * 
+   FROM characteristics
+  WHERE wesen_characteristics LIKE '%bull%' OR wesen_characteristics LIKE '%fox%';
+ 
+-- Query for which month in each year had the most wesen introductions
+WITH dates AS (SELECT wesen_id, MONTHNAME(air_date) AS time_of_year, EXTRACT(YEAR FROM air_date) AS YEAR, EXTRACT(MONTH FROM air_date) AS month
+  FROM episodes
+ ORDER BY air_date, season_num)
+ 
+SELECT time_of_year, count(time_of_year) AS number_of_wesen_first_appearances, year
+FROM dates
+WHERE YEAR = 2011 
+GROUP BY month
+ORDER BY number_of_wesen_first_appearances desc
+LIMIT 1
  
 
 
